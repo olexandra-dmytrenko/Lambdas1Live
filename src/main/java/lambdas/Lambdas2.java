0ef0c99cf4b6064cc3221812044dfc921f2bad4c
Olexandra <olexandra.dmytrenko@gmail.com>
2016-01-18 21:37:21 +0200

    Sout + IntStream

--- src/main/java/lambdas/Lambdas2.java
+++ src/main/java/lambdas/Lambdas2.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -18,6 +19,11 @@ public class Lambdas2 {
         System.out.println(getDoubledEvenGT3(numbers));
         System.out.println(getDoubledEvenGT3Lambdas(numbers));
         System.out.println(getDoubledEvenGT3LambdasList(numbers));
+        getDoubledEvenGT3LambdasList(numbers).stream().forEach(System.out::println);
+        getDoubledEvenGT3LambdasList(numbers).stream().forEach(e ->
+        {
+            System.out.println(e);
+        });
     }
 
     public static int getDoubledEvenGT3(List<Integer> numbers) {
@@ -68,11 +74,19 @@ public class Lambdas2 {
     private static Predicate<Integer> isEven() {
         return number -> number % 2 == 0;
     }
+
     //    private static Predicate<Integer> isGreaterThan3() {
 //        return number -> number > 3;
 //    }
     private static boolean isGreaterThan3(int number) {
         System.out.println("sGreaterThan3 " + number);
         return number > 3;
+    }
+
+    private void useIntStream() {
+        IntStream.of(1, 2, 5, 4, 6, 5, 4, 3, 8)
+                .filter(number -> number > 3)
+                .filter(number -> number % 2 == 0)
+                .map(Lambdas2::getDoubled).findFirst();
     }
 }
